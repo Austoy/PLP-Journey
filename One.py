71dd33d3f73e27ee71531a6559c187b49e622f52
Austoy <austinkark@gmail.com>
2024-03-06 12:21:44 +0300

    Control Flow in Python   Changes to be committed:  new file:   Control Flow.py 	new file:   One.py

---
+++ One.py
@@ -0,0 +1,22 @@
+marks = 80
+result = ""
+if marks < 30:
+  result = "Failed"
+elif marks > 75:
+  result = "Passed with distinction"
+else:
+  result = "Passed"
+
+  print(result)
+
+  def checkVowel(n): 
+   match n: 
+	case 'a': return "Vowel alphabet" 
+	case 'e': return "Vowel alphabet" 
+	case 'i': return "Vowel alphabet" 
+	case 'o': return "Vowel alphabet" 
+	case 'u': return "Vowel alphabet" 	
+	case _: return "Simple alphabet" 	
+print(checkVowel('a')) 
+print(checkVowel('m')) 
+print(checkVowel('o'))
