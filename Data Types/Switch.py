47e5be88af97c2b0f3d012313937f23ad60f2939
Austoy <austinkark@gmail.com>
2024-03-06 16:06:36 +0300

    Loops and Jump statements

---
+++ Data Types/Switch.py
@@ -0,0 +1,13 @@
+
+class Python_Switch: 
+
+  def day(self,month):
+   default = "Incorrect day"
+   return getattr(self,'case_' + str(month),lambda: default)()
+  def case_1(self):
+     return "Jan"
+  def case_2(self):
+     return "Feb"
+  def case_3(self):
+     return "Mar"
+
