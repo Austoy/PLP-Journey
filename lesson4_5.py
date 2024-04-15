42f88175f5278b7ff628f2b2e029f903da2c0490
Austoy <austinkark@gmail.com>
2024-04-15 06:21:59 +0300

    Recent commits from PLP python.

---
+++ lesson4_5.py
@@ -0,0 +1,20 @@
+integer_number = 123
+float_number = 1.23
+new_number = integer_number + float_number
+
+print("value:", new_number)
+print("data type:", type(new_number))
+
+a = (1, 2, 3)
+
+b = list(a)
+
+print(b)
+
+x ={}
+x[2] = 10
+x[1] = [20,30,40]
+print(x[1][2])
+
+name = testing
+print(len(name))
