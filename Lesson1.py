750a467f5bb06e9deb4914fa4f76d62dbac7cce5
Austoy <austinkark@gmail.com>
2025-03-23 17:31:18 +0300

    initial commit

---
+++ Lesson1.py
@@ -0,0 +1,14 @@
+#const add Numbers = (a, b) => {
+# return (a + b);
+#};
+
+
+#const a =10;
+#const b = 5;
+#console.log(addNumbers(a, b));
+
+
+
+a = 10
+b = 5
+print(a + b)
---
+++ lesson2_3.py
@@ -0,0 +1,12 @@
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
+
+languages = ["Swift", "Java", "Python"]
+print(languages[0])
+print(languages[2])
